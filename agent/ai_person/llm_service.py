5b236414a688ba2d023ba0af7c43891e2b7943dd
ujjalsharma100 <ujjalsharma100@gmail.com>
2025-05-31 16:56:39 +0530

    added ollama support in llm service

--- agent/ai_person/llm_service.py
+++ agent/ai_person/llm_service.py
@@ -1,8 +1,17 @@
 from anthropic import Anthropic
+import ollama
 
 client = Anthropic(api_key="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")
 
+llm_choice = 'anthropic'
+
 def get_response_from_llm(prompt: str) -> str:
+    if llm_choice == 'anthropic':
+        return get_response_from_anthropic(prompt=prompt)
+    elif llm_choice == 'ollama':
+        return get_response_from_ollama(prompt=prompt)
+
+def get_response_from_anthropic(prompt: str) -> str:
     try:
         print("prompt:\n" + prompt)
         # Call Claude API
@@ -23,5 +32,19 @@ def get_response_from_llm(prompt: str) -> str:
         print("response_text:\n" + response_text)
         return response_text
 
+    except Exception as e:
+        print(e)
+
+def get_response_from_ollama(prompt: str) -> str:
+    try:
+        print("prompt:\n" + prompt)
+        
+        response = ollama.chat(model="llama3.1", messages=[{"role": "user", "content": prompt}])
+        
+        print("llm response: \n")
+        print(response)
+        response_text = response['message']['content']
+        print("response_text:\n" + response_text)
+        return response_text
     except Exception as e:
         print(e)
